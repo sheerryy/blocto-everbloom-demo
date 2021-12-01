1667aacb8b4d12bcb6cd2401e1eec1124d6a0a88
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-12-01 19:50:53 +0500

    remove comments

--- src/SignAndVerify.js
+++ src/SignAndVerify.js
@@ -46,25 +46,3 @@ const SignAndVerify = () => {
 };
 
 export default SignAndVerify;
-
-// marketplace-contracts-resarch-and-question-gathering:
-// - Studied existing market contracts Topshot, Storfront, Topshot & Rarible
-// Integrate Wallet API webhook
-// - Migrate the logic for all existing transaction
-// Blocto API to allow showcasing of NFTS
-// - Can easily show Everbloom NFTs, will have to handle cases for each marketpalces
-// Investigate Mixed Custodial wallet
-// - We can give user function to move to non custodial solution
-// - They cannot add the account to blocto unless blocto provides any Functionality on this
-// - Wallet is just a management tool for account
-// - We can implement custom signing function but user will have to sign with his private key
-// NFT standard update
-// - Flow team is not making any progress on NFT standards
-// - I am not sure if they are going to change it
-// - However we can implement Generic Interfaces for Metadata
-// - Should we upgrade the standard before getting them approved?
-// Discord bot
-// - We can create a custom webpage to verify user account NFT
-// - then can invite to a private channel or just that user a (VIP_USER) role in the backend
-// - We can also implement revealing the signature part like this
-
